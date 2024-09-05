121a74c53afa7edae44b639f54def60afb6e30e2
Aryan <aryannayar47@gmail.com>
2024-09-05 22:48:42 +0530

    Added product fetch from backend functionality

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,12 +1,30 @@
-import { pizzas } from "@/data";
+
+import { ProductType } from "@/types/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+type Props={
+  params: {
+    category: string;
+  };
+}
 
-const CategoryPage = () => {
+const getData=async(category:string)=>{
+  const res=await fetch(`http://localhost:3000/api/products?cat=${category}`,{
+    cache:"no-store"
+  })
+
+  if(!res.ok){
+    throw new Error(`Failed to fetch data: ${res.status}`)
+  }
+  return res.json()
+}
+
+const CategoryPage = async({params}:Props) => {
+  const products:ProductType[]=await getData(params.category)
   return (
     <div className="flex flex-wrap text-red-500">
-      {pizzas.map((item) => (
+      {products.map((item) => (
         <Link className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50" href={`/product/${item.id}`} key={item.id}>
           {/* IMAGE CONTAINER */}
           {item.img && (
@@ -17,7 +35,7 @@ const CategoryPage = () => {
           {/* TEXT CONTAINER */}
           <div className="flex items-center justify-between font-bold">
             <h1 className="text-2xl uppercase p-2">{item.title}</h1>
-            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+            <h2 className="group-hover:hidden text-xl">INR {item.price}</h2>
             <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">Add to Cart</button>
           </div>
         </Link>
