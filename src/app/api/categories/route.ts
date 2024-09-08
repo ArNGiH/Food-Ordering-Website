a57ab3f063ba358cb84bf339d99114b92a0741cd
Aryan <aryannayar47@gmail.com>
2024-09-08 13:09:18 +0530

    Handled modelling of orders page

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../products/utils/connect";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+
 
 export const GET = async () => {
   try {
