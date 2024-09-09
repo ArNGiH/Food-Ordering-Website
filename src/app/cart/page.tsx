76913694e2337cd2c1eacc0efcb61a6509a71d39
Aryan <aryannayar47@gmail.com>
2024-09-09 18:36:23 +0530

    Changes made to cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,14 +1,42 @@
-"use client"
+"use client";
 import { useCartStore } from "@/utils/store";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 const CartPage = () => {
   const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
+  const { data: session } = useSession();
+  const router = useRouter();
 
-  useEffect(()=>{
-    useCartStore.persist.rehydrate
-  },[])
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+  }, []);
+
+  const handleCheckout = async () => {
+    if (!session) {
+      router.push("/login");
+    } else {
+      try {
+        const res = await fetch("http://localhost:3000/api/orders", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            price: totalPrice,
+            products,
+            status: "Not Paid!",
+            userEmail: session.user.email,
+          }),
+        });
+        const data =await res.json()
+        router.push(`/pay/${data.id}`)
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
       {/* PRODUCTS CONTAINER */}
@@ -35,12 +63,11 @@ const CartPage = () => {
           </div>
         ))}
       </div>
-  
       {/* PAYMENT CONTAINER */}
       <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
         <div className="flex justify-between">
-          <span className="">Subtotal ({totalItems}))</span>
-          <span className="">INR {totalPrice}</span>
+          <span className="">Subtotal ({totalItems} items)</span>
+          <span className="">${totalPrice}</span>
         </div>
         <div className="flex justify-between">
           <span className="">Service Cost</span>
@@ -53,9 +80,12 @@ const CartPage = () => {
         <hr className="my-2" />
         <div className="flex justify-between">
           <span className="">TOTAL(INCL. VAT)</span>
-          <span className="font-bold">INR {totalPrice}</span>
+          <span className="font-bold">${totalPrice}</span>
         </div>
-        <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
+        <button
+          className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end"
+          onClick={handleCheckout}
+        >
           CHECKOUT
         </button>
       </div>
