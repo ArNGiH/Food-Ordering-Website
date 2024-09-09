3472c7b02a0bdd477bf6364801e10c3b67087c38
Aryan <aryannayar47@gmail.com>
2024-09-09 18:31:11 +0530

    Made changes to navbar

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,24 +1,42 @@
 "use client"
+import { useCartStore } from "@/utils/store";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect } from "react";
 
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useCartStore } from '@/utils/store'
-import { useEffect } from 'react'
 const CartIcon = () => {
-  const {totalItems}=useCartStore()
+  const { data: session, status } = useSession();
+
+  const { totalItems } = useCartStore();
+
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+  }, []);
+
   useEffect(()=>{
-    useCartStore.persist.rehydrate
+    useCartStore.persist.rehydrate()
   },[])
   return (
+    <Link href={session?.user.isAdmin ? "/add" : "/cart"}>
+      <div className="flex items-center gap-4">
+        <div className="relative w-8 h-8 md:w-5 md:h-5">
+          <Image
+            src="/cart.png"
+            alt=""
+            fill
+            sizes="100%"
+            className="object-contain"
+          />
+        </div>
+        {session?.user.isAdmin ? (
+          <button className="p-1 bg-red-500 text-white rounded-md">Add product</button>
+        ) : (
+          <span>Cart ({totalItems})</span>
+        )}
+      </div>
+    </Link>
+  );
+};
 
-   <Link href="/cart" className='flex items-center gap-4'>
-    <div className='relative w-8 h-8  md:w-5 md:h-5'>
-        <Image src='/cart.png' alt='' fill/>
-    </div>
-    <span>Cart [{totalItems}]</span>
-   </Link>
-  )
-}
-
-export default CartIcon
+export default CartIcon;
